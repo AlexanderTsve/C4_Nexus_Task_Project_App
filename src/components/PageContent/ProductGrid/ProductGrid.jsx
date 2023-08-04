239551398f1f58091de8180560ce495f303cede4
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-08-04 17:34:22 +0300

    Worked on the product grid component - styles and logic.

--- src/components/PageContent/ProductGrid/ProductGrid.jsx
+++ src/components/PageContent/ProductGrid/ProductGrid.jsx
@@ -1,18 +1,29 @@
-import Button from "react-bootstrap/Button";
+import Button from "../../Button/Button";
 import Card from "react-bootstrap/Card";
+import styles from "./ProductGrid.module.scss";
 const ProductGrid = ({ arrOfProducts }) => {
   return (
-    <section>
+    <section className={styles["products-container"]}>
       {arrOfProducts.map((product, index) => {
         return (
-          <Card style={{ width: "18rem" }} key={index}>
+          <Card key={index} className={styles.container}>
             <Card.Img
+              style={{ width: "18rem", textAlign: "center" }}
               variant="top"
               src={require(`../../../assets/images/${product.name}.png`)}
             />
-            <Card.Body>
-              <Card.Title>Title: {product.name}</Card.Title>
-              <Card.Text>Description: {product.description}</Card.Text>
+            <Card.Body className={styles.content}>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text className={styles["content-description"]}>
+                {product.description}
+              </Card.Text>
+              <Card.Text>Metal: {product.metal}</Card.Text>
+              <Card.Text>
+                {product.price.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </Card.Text>
               <Button variant="primary">Add to cart</Button>
             </Card.Body>
           </Card>
