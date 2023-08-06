a8fe542a7307beb0d4397f26d7efc606d936f10a
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-08-06 17:14:03 +0300

    ProductCard component created.

---
+++ src/components/PageContent/ProductCard/ProductCard.jsx
@@ -0,0 +1,33 @@
+import Card from "react-bootstrap/Card";
+import Button from "../../Button/Button";
+import RatingSystem from "../../RatingSystem/RatingSystem";
+import styles from "./ProductCard.module.scss";
+const ProductCard = ({ index, name, description, metal, price }) => {
+  return (
+    <Card key={index} className={styles.container}>
+      <Card.Img
+        style={{ width: "18rem", textAlign: "center" }}
+        variant="top"
+        src={require(`../../../assets/images/${name}.png`)}
+      />
+      <Card.Body className={styles.content}>
+        <Card.Title className={styles["content-title"]}>{name}</Card.Title>
+        <Card.Text className={styles["content-description"]}>
+          {description}
+        </Card.Text>
+        <Card.Text className={styles["content-description"]}>
+          Metal: {metal}
+        </Card.Text>
+        <Card.Text className={styles["content-price"]}>
+          {price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </Card.Text>
+        <RatingSystem />
+        <Button variant="primary">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+export default ProductCard;
